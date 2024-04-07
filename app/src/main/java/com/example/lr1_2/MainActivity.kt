9b8f3c8d2b22625e3c02d83e94fc7a81139f1ffd
flamebrier <flamebrier@gmail.com>
2024-04-07 20:38:01 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lr1_2/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.lr1_2
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private val tag = "States"
+    private var btnActTwo: Button? = null
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnActTwo = findViewById<View>(R.id.btn_go_to) as Button
+        btnActTwo!!.setOnClickListener(this)
+        Log.d(tag, "MainActivity: onCreate()")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(tag, "MainActivity: onRestart()")
+    }
+    override fun onStart() {super.onStart()
+        Log.d(tag, "MainActivity: onStart()")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(tag, "MainActivity: onResume()")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(tag, "MainActivity: onPause()")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(tag, "MainActivity: onStop()")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(tag, "MainActivity: onDestroy()")
+    }
+    override fun onTap(v: View) {
+        val intent = Intent(this, ActivityTwo::class.java)
+        startActivity(intent)
+    }
+}
