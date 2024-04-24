090375ea0fe12fc22ad939c685473ef386e4ff02
arsLan4k1390 <hellboy.001@mail.ru>
2024-04-25 01:07:45 +0500

    Фикс всего проекта
    1. Хардкодная локализация была удалена и все переводы были перенесены в strings.xml
    2. Был создан файл с русской локализацией
    3. Был исправлен onTap на onClick в MainActivity
    4. В ActivityTwo слегка было изменено логирование, чтобы не путаться с MainActivity
    5. В манифесте исправлен "activity" второго активити, а также интент фильтр
    6. В activity_main убрал скрытие текста "Привет, мир!"
    7. Для красоты кода были добавлены абзацы

--- app/src/main/java/com/example/lr1_2/ActivityTwo.kt
+++ app/src/main/java/com/example/lr1_2/ActivityTwo.kt
@@ -1,44 +1,55 @@
 package com.example.lr1_2
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+
 class ActivityTwo : AppCompatActivity(), View.OnClickListener {
+
     private val tag = "States"
     private var btnActTwo: Button? = null
+
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContentView(R.layout.activity_two)
         btnActTwo = findViewById<View>(R.id.btn_go_to) as Button
         btnActTwo!!.setOnClickListener(this)
-        Log.d(tag, "MainActivity: onCreate()")
+        Log.d(tag, "ActivityTwo: onCreate()")
     }
+
     override fun onRestart() {
         super.onRestart()
-        Log.d(tag, "MainActivity: onRestart()")
+        Log.d(tag, "ActivityTwo: onRestart()")
     }
-    override fun onStart() {super.onStart()
-        Log.d(tag, "MainActivity: onStart()")
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(tag, "ActivityTwo: onStart()")
     }
+
     override fun onResume() {
         super.onResume()
-        Log.d(tag, "MainActivity: onResume()")
+        Log.d(tag, "ActivityTwo: onResume()")
     }
+
     override fun onPause() {
         super.onPause()
-        Log.d(tag, "MainActivity: onPause()")
+        Log.d(tag, "ActivityTwo: onPause()")
     }
+
     override fun onStop() {
         super.onStop()
-        Log.d(tag, "MainActivity: onStop()")
+        Log.d(tag, "ActivityTwo: onStop()")
     }
+
     override fun onDestroy() {
         super.onDestroy()
-        Log.d(tag, "MainActivity: onDestroy()")
+        Log.d(tag, "ActivityTwo: onDestroy()")
     }
+
     override fun onClick(v: View) {
         finish()
     }
--- app/src/main/java/com/example/lr1_2/MainActivity.kt
+++ app/src/main/java/com/example/lr1_2/MainActivity.kt
@@ -8,39 +8,50 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
     private val tag = "States"
     private var btnActTwo: Button? = null
+
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
         btnActTwo = findViewById<View>(R.id.btn_go_to) as Button
         btnActTwo!!.setOnClickListener(this)
         Log.d(tag, "MainActivity: onCreate()")
     }
+
     override fun onRestart() {
         super.onRestart()
         Log.d(tag, "MainActivity: onRestart()")
     }
-    override fun onStart() {super.onStart()
+
+    override fun onStart() {
+        super.onStart()
         Log.d(tag, "MainActivity: onStart()")
     }
+
     override fun onResume() {
         super.onResume()
         Log.d(tag, "MainActivity: onResume()")
     }
+
     override fun onPause() {
         super.onPause()
         Log.d(tag, "MainActivity: onPause()")
     }
+
     override fun onStop() {
         super.onStop()
         Log.d(tag, "MainActivity: onStop()")
     }
+
     override fun onDestroy() {
         super.onDestroy()
         Log.d(tag, "MainActivity: onDestroy()")
     }
-    override fun onTap(v: View) {
+
+    override fun onClick(v: View) {
         val intent = Intent(this, ActivityTwo::class.java)
         startActivity(intent)
     }
